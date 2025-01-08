f95dbda508736ff37e748ee9a490d465f9ec336b
aaftab.ansari <aaftab.ansari@crestinfotech.co.in>
2025-01-08 17:47:16 +0530

    new changes

---
+++ src/components/PokemonFlatList.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import {
+  FlatList,
+  SectionList,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import pokemonList from '../data/data.json';
+import groupedPokemonList from '../data/pokemonGroupData.json';
+
+const PokemonFlatList = () => {
+  const renderItem = ({
+    item,
+  }: {
+    item: {id: number; type: string; name: string};
+  }) => {
+    console.log(item.id);
+    return (
+      <View style={styles.card}>
+        <Text style={styles.cardText}>{item.type}</Text>
+        <Text style={styles.cardText}>{item.name}</Text>
+      </View>
+    );
+  };
+
+  const renderSeparator = () => <View style={{height: 16}} />;
+
+  return (
+    // <View style={styles.scrollView}>
+    //   <FlatList
+    //     data={pokemonList}
+    //     renderItem={renderItem}
+    //     keyExtractor={item => item.id.toString()}
+    //     ItemSeparatorComponent={renderSeparator}
+    //     ListEmptyComponent={<Text>No Items Found</Text>}
+    //     ListHeaderComponent={
+    //       <Text style={styles.headerText}>Pokemon List</Text>
+    //     }
+    //     ListFooterComponent={<Text style={styles.footerText}>End of list</Text>}
+    //     // horizontal={true}
+    //   />
+    // </View>
+    <View style={styles.scrollView}>
+      <SectionList
+        sections={groupedPokemonList}
+        renderItem={({item}) => {
+          return (
+            <View style={styles.card}>
+              <Text style={styles.cardText}>{item}</Text>
+            </View>
+          );
+        }}
+        renderSectionHeader={({section}) => (
+          <Text style={styles.sectionHeaderText}>{section.type}</Text>
+        )}
+        ItemSeparatorComponent={() => (
+          <View
+            style={{
+              height: 16,
+            }}
+          />
+        )}
+        SectionSeparatorComponent={() => (
+          <View
+            style={{
+              height: 16,
+            }}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: '#FFFFFF',
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  cardText: {
+    fontSize: 30,
+  },
+  headerText: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 12,
+  },
+  footerText: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 12,
+  },
+  sectionHeaderText: {
+    backgroundColor: '#FFFFFF',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default PokemonFlatList;
